f3b9ccb647bf7335109061115c1a8562303b7fc7
Dheeraj <410dheeraj@gmail.com>
2020-08-16 22:07:52 +0530

    (logo): updated wrapper style
    - made logo non selectable
    - added hover effects

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo = () => {
 	return (
-		<div className='w-16 h-16 py-2 px-2'>
+		<div className='p-2 hover:bg-gray-700 select-none'>
 			<p className='text-zeal font-semibold text-2xl'>ZEAL</p>
 			<p className='text-zeal font-logo text-xl'>Academy</p>
 		</div>
