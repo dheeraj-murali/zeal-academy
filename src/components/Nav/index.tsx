03aec412e992128bdd72b62d529bcede339968fe
Dheeraj <410dheeraj@gmail.com>
2020-08-16 22:34:09 +0530

    feat(smooth-scroll): adding smooth scroll

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { links } from '../../utils/config/navLinks';
+import Link from 'react-anchor-link-smooth-scroll';
 
 export const Nav = () => {
 	return (
 		<>
 			<div className='py-2 px-2'>
 				{links.map((link, index) => (
-					<a href={link.link} className='py-2 px-4 hover:text-zeal'>
+					<Link
+						offset='100'
+						href={link.link}
+						key={index}
+						className='text-base py-2 px-4 hover:text-zeal'
+					>
 						{link.text}
-					</a>
+					</Link>
 				))}
 			</div>
-			<div className='py-2 px-2 hidden md:block'>
-				Questions? Call us at +91 988765432
-			</div>
+			<a
+				href='tel:+91988765432'
+				className='py-2 px-2 hidden md:block hover:text-zeal'
+			>
+				Questions? &nbsp; Call us at +91 988765432
+			</a>
 		</>
 	);
 };
