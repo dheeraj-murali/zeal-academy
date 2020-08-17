e5fc8ae4bd3dae09ab607d1b01bd8a4f92267abe
Dheeraj <410dheeraj@gmail.com>
2020-08-17 08:30:14 +0530

    ci(purge): adding force purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	purge: [
-		'src/**/*.js',
-		'src/**/*.jsx',
-		'src/**/*.ts',
-		'src/**/*.tsx',
-		'public/**/*.html',
-	],
+	purge: {
+		enabled: true,
+		content: [
+			'src/**/*.js',
+			'src/**/*.jsx',
+			'src/**/*.ts',
+			'src/**/*.tsx',
+			'public/**/*.html',
+		],
+	},
 	theme: {
 		extend: {
 			fontFamily: {
