481c27e7c8f696708e661dc78b715756bfa4fdbe
Dheeraj <410dheeraj@gmail.com>
2020-08-17 10:58:10 +0530

    refactor(HTML): added proper html tags

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,18 +5,21 @@ import Link from 'react-anchor-link-smooth-scroll';
 export const Nav = () => {
 	return (
 		<>
-			<div className='py-2 px-2'>
-				{links.map((link, index) => (
-					<Link
-						offset='100'
-						href={link.link}
-						key={index}
-						className='text-base py-2 px-4 hover:text-zeal'
-					>
-						{link.text}
-					</Link>
-				))}
-			</div>
+			<nav className='py-2 px-2'>
+				<ul className='space-x-2 md:space-x-6'>
+					{links.map((link, index) => (
+						<li key={index} className='float-left'>
+							<Link
+								offset='100'
+								href={link.link}
+								className='text-base hover:text-zeal'
+							>
+								{link.text}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
 			<a
 				href='tel:+91988765432'
 				className='py-2 px-2 hidden md:block hover:text-zeal'
