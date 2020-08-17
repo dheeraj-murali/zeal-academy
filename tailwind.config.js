dd8fd51d45d97a4265e1ffb5ee1c7aa1e538f45f
Dheeraj <410dheeraj@gmail.com>
2020-08-17 10:16:31 +0530

    perf(images): optimized images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
 	purge: {
-		enabled: true,
+		enabled: process.env.NODE_ENV === 'production',
 		content: [
 			'src/**/*.js',
 			'src/**/*.jsx',
