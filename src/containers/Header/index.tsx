bb412b4d992aaf198811e9bed2cc28750ef39158
Dheeraj <410dheeraj@gmail.com>
2020-08-17 10:59:46 +0530

    refactor(html): added proper html tags

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -3,9 +3,9 @@ import { Logo, Nav } from '../../components';
 
 export const Header = () => {
 	return (
-		<div className='absolute flex justify-around items-center z-30 w-screen h-20 text-white'>
+		<header className='absolute flex justify-around items-center z-30 w-screen h-20 text-white'>
 			<Logo />
 			<Nav />
-		</div>
+		</header>
 	);
 };
