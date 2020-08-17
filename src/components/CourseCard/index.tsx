093c34c907fdfab6d9a6deaf13db2be92d13d498
Dheeraj <410dheeraj@gmail.com>
2020-08-17 10:57:38 +0530

    refactor(alt): dynamic alt prop
    - updated styles for consitancy

--- src/components/CourseCard/index.tsx
+++ src/components/CourseCard/index.tsx
@@ -3,18 +3,15 @@ import React from 'react';
 export const CourseCard = (props: CourseCardProps) => {
 	const { course } = props;
 	return (
-		<div
-			className='min-w-full mx-4 rounded-lg border border-gray-400 overflow-hidden shadow-lg bg-white'
-			// style={{ minWidth: '24rem' }}
-		>
+		<div className='mx-4 rounded-lg border border-gray-400 overflow-hidden shadow-lg bg-white h-full'>
 			<img
 				className='w-full'
 				src={course.image}
-				alt='Sunset in the mountains'
+				alt={`${course.title}`}
 			/>
 			<div className='space-y-3 px-6 py-4'>
-				<p className='text-blue-700 text-sm'>{course.span}</p>
-				<div className='font-semibold text-xl mb-2'>{course.title}</div>
+				<span className='text-blue-700 text-sm'>{course.span}</span>
+				<h3 className='font-semibold text-xl mb-2'>{course.title}</h3>
 				<p className='text-gray-700 text-base'>{course.description}</p>
 			</div>
 		</div>
