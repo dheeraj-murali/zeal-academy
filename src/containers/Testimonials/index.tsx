f5fc2cfb3a541dba305aa422305a5e60d57d679c
Dheeraj <410dheeraj@gmail.com>
2020-08-18 22:41:11 +0530

    style(card): spacing
    - adjusted card size
    - updated font size
    - update responsiveness

--- src/containers/Testimonials/index.tsx
+++ src/containers/Testimonials/index.tsx
@@ -10,15 +10,15 @@ export const Testimonials = () => {
 			className='flex flex-col justify-center content-center w-screen bg-scroll bg-no-repeat bg-contain bg-bottom'
 			style={{ backgroundImage: `url(${wave})` }}
 		>
-			<div className='flex flex-wrap my-10 py-10'>
-				<div className='flex flex-col justify-center text-center space-y-8 w-full pl-8 md:pl-24 pr-8 lg:px-24'>
-					<h2 className='text-2xl font-bold antialiased text-current'>
+			<div className='flex flex-wrap my-10 py-10 space-y-10'>
+				<div className='flex flex-col justify-center text-center space-y-10 w-full pl-8 md:pl-24 pr-8 lg:px-24'>
+					<h2 className='text-4xl font-bold antialiased text-current'>
 						What our students are saying
 					</h2>
 					<Arrow />
 				</div>
 				<div className='inline-flex w-screen z-10 space-x-8 overflow-x-scroll overscroll-x-contain p-6'>
-					<ul className='inline-flex w-screen md:w-1/4 space-x-2 md:space-x-10 md:ml-40'>
+					<ul className='inline-flex w-screen md:w-2/4 lg:w-2/6 xl:w-1/4 space-x-2 md:space-x-10 md:ml-40'>
 						{testimonials.map((testimonial, index) => (
 							<li key={index} className='min-w-full'>
 								<TestimonialCard person={testimonial} />
