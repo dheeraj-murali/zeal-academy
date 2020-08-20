931068ba922935d576dde41d322f88c4d9bdef17
Dheeraj <410dheeraj@gmail.com>
2020-08-20 19:49:31 +0530

    ci(data): Updated config data

--- src/utils/config/testimonial.ts
+++ src/utils/config/testimonial.ts
@@ -1,39 +1,45 @@
-import { ajay, sameer, haritha, maria, anto } from '../../images';
+import { ajay, arya, naina, anjana, amal, haritha } from '../../images';
 
 export const testimonials = [
 	{
 		name: 'Ajay Manimala',
-		qualification: 'KAPS Qualifier',
+		qualification: 'Intern pharmacist',
 		message:
-			'I must appreciate the effort of each and every faculty and with their help learning for this exam was lot more easier...',
+			'I must appreciate the effort of each and every faculty at ZEAL Academy, with their help preparing for this exam was lot more easier...',
 		photo: ajay,
 	},
 	{
-		name: 'Sameer KM',
+		name: 'Arya Jacob',
 		qualification: 'KAPS Qualifier',
 		message:
-			'It was really a great experience. The coaching they provide is so helpful for the exam preparation. With your help I passed the exam...',
-		photo: sameer,
+			'It was really a great experience. The coaching they have provide is so helpful for the exam preparation. Thank you guys, With your help I was able to pass the exam...',
+		photo: arya,
+	},
+	{
+		name: 'Anjana Balakrishnan',
+		qualification: 'Intern pharmacist',
+		message: `Joining ZEAL Academy was easily the best choice I've taken in my life, I thank them for helping me in landing this job.`,
+		photo: anjana,
 	},
 	{
 		name: 'Haritha Alexander',
-		qualification: 'KAPS Qualifier',
+		qualification: 'Intern pharmacist',
 		message:
-			'I am from Australia and took online coaching from this academy. It has really helped me passing this exam...',
+			'I am from Australia and took online coaching from ZEAL Academy. It was really helped me  pass the exams...',
 		photo: haritha,
 	},
 	{
-		name: 'Maria Shaji',
+		name: 'Amal Sebastian',
 		qualification: 'KAPS Qualifier',
 		message:
-			'I passed KAPS exam with their help.They do step down in clearing all doubts and help in understanding better. Happy that I gave it a try and joined Zeal academy...',
-		photo: maria,
+			'I passed KAPS with their help. They do step down in clearing all doubts and helped me in understanding the concepts better. Happy that I gave it a try and joined ZEAL Academy...',
+		photo: amal,
 	},
 	{
-		name: 'Anto John',
+		name: 'Naina Elizabeth',
 		qualification: 'KAPS Qualifier',
 		message:
-			'I am really grateful to Zeal academy for all your effort in helping me pass KAPS exam..',
-		photo: anto,
+			'I am really grateful to ZEAL Academy for all your effort in helping me pass KAPS exam..',
+		photo: naina,
 	},
 ];
