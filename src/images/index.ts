67ab2770838cdaf42632fa565ace3d6ee321e378
Dheeraj <410dheeraj@gmail.com>
2020-08-20 22:04:47 +0530

    feat(images): updated webp images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,5 +1,3 @@
-import about from './about.webp';
-import arrow from './arrow.webp';
 import banner1 from './banners/banner1.webp';
 import banner2 from './banners/banner2.webp';
 import banner3 from './banners/banner3.webp';
@@ -7,14 +5,23 @@ import banner4 from './banners/banner4.webp';
 import banner5 from './banners/banner5.webp';
 import banner6 from './banners/banner6.webp';
 import banner7 from './banners/banner7.webp';
-import hero from './hero.webp';
+import anjali from './team/anjali.webp';
+import anphil from './team/anphil.webp';
+import anu from './team/anu.webp';
+import christeena from './team/christeena.webp';
+import jeeva from './team/jeeva.webp';
+import karthika from './team/karthika.webp';
+import lithin from './team/lithin.webp';
 import ajay from './testimonials/ajay.webp';
 import amal from './testimonials/amal.webp';
 import anjana from './testimonials/anjana.webp';
 import arya from './testimonials/arya.webp';
 import haritha from './testimonials/haritha.webp';
 import naina from './testimonials/naina.webp';
-import wave from './wave.webp';
+import about from './web/about.webp';
+import arrow from './web/arrow.webp';
+import hero from './web/hero.webp';
+import wave from './web/wave.webp';
 
 export {
 	about,
@@ -34,4 +41,11 @@ export {
 	haritha,
 	anjana,
 	naina,
+	anjali,
+	anphil,
+	anu,
+	christeena,
+	jeeva,
+	karthika,
+	lithin,
 };
