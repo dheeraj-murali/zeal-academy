13bb36c76bb5da27b0e0bfeaebc3a098087d08f3
Dheeraj <410dheeraj@gmail.com>
2020-08-21 21:43:46 +0530

    feat(call): call button for mobile

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'react-anchor-link-smooth-scroll';
+import { FaPhoneAlt } from 'react-icons/fa';
 
 export const Nav = (props: NavProps) => {
 	const { links, phone } = props;
@@ -21,12 +22,20 @@ export const Nav = (props: NavProps) => {
 				</ul>
 			</nav>
 			{phone && (
-				<a
-					href={`tel:${phone}`}
-					className='py-2 px-2 hidden md:block hover:text-zeal'
-				>
-					Questions? &nbsp; Call us at {phone}
-				</a>
+				<>
+					<a
+						href={`tel:${phone}`}
+						className='py-2 px-2 hidden md:block hover:text-zeal'
+					>
+						Questions? &nbsp; Call us at {phone}
+					</a>
+					<a
+						href={`tel:${phone}`}
+						className='py-2 px-2 md:hidden hover:text-zeal'
+					>
+						<FaPhoneAlt />
+					</a>
+				</>
 			)}
 		</>
 	);
