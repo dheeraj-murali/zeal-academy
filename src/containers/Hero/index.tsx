cf3b268fde47a341e0b425396cca00fb7795617b
Dheeraj <410dheeraj@gmail.com>
2020-08-23 12:26:26 +0530

    feat(config background): added ability to config background

--- src/containers/Hero/index.tsx
+++ src/containers/Hero/index.tsx
@@ -7,7 +7,7 @@ export default function () {
 		<section
 			className='h-screen w-screen bg-fixed bg-no-repeat bg-cover bg-center'
 			style={{
-				backgroundImage: `linear-gradient(0deg, rgba(41,216,119,0.4) 0%, rgba(51, 51, 51, 0.4) 39.84%), url(${config.background})`,
+				backgroundImage: `${config.background}`,
 			}}
 		>
 			<div className='flex flex-col h-screen w-screen justify-center px-8 md:px-24 lg:px-32'>
--- src/utils/config/hero.ts
+++ src/utils/config/hero.ts
@@ -1,6 +1,6 @@
-import { hero as heroBanner } from '../../images';
+import { hero as banner } from '../../images';
 
 export const hero = {
 	title: `Learn from the best,\nTurn your dreams into reality.`,
-	background: heroBanner,
+	background: `linear-gradient(0deg, rgba(41,216,119,0.4) 0%, rgba(51, 51, 51, 0.4) 39.84%), url(${banner})`,
 };
